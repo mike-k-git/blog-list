350a5d789200b02ed3730431b9c8a06388c1cc92
mike-k-git <mikehsm@outlook.com>
2022-04-11 21:03:00 +0300

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
+const morgan = require('morgan')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch((error) => logger.error('error connecting to MongoDB', error.message))
+
+app.use(cors())
+app.use(express.json())
+morgan.token('body', (request) => JSON.stringify(request.body))
+app.use(
+  morgan(
+    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :body'
+  )
+)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
