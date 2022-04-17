aabdb8b129c0712ff1efd44b14575ce1f8989401
mike-k-git <mikehsm@outlook.com>
2022-04-17 13:23:25 +0300

    part5

--- controllers/login.js
+++ controllers/login.js
@@ -10,7 +10,6 @@ loginRouter.post('/', async (request, response) => {
   const passwordCorrect =
     user === null ? false : await bcrypt.compare(password, user.passwordHash)
 
-  console.log(user, passwordCorrect)
   if (!(user && passwordCorrect)) {
     return response.status(401).json({ error: 'invalid username or password' })
   }
